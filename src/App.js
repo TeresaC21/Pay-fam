92b9991a65c6f269472166b411f4ce7a2e74aa13
Tezzi Carbajal <tezzi.cah@gmail.com>
2020-07-07 22:15:50 -0500

    Create condition for show some components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // Components
 import Question from './components/Question';
+import Form from './components/Form';
 
 function App() {
+  // Define the states
+  const [budget, setBudget] = useState(0);
+  const [rest, setRest] = useState(0);
+  const [showQuestion, setShowQuestion] = useState(true)
+
+
   return (
     <div className="container">
       <h1>Presupuesto</h1>
 
       <div className="content-main content">
-        <Question />
+        {showQuestion ?
+          (
+            <Question
+              setBudget={setBudget}
+              setRest={setRest}
+              setShowQuestion={setShowQuestion}
+            />
+          ) : (
+            <div className="row">
+              <div className="one-half column">
+                <Form />
+              </div>
+              <div className="one-half column">
+                2
+            </div>
+            </div>
+          )}
       </div>
-
     </div>
   );
 }
